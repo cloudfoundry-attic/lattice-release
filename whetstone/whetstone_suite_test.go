246a910f89a4faa29d89dff7209340b57731f8a4
Matthew McNew <mmcnew@pivotal.io>
2014-10-17 14:54:22 -0500

    initial test deploying a docker app
    [#80699410]

--- whetstone/whetstone_suite_test.go
+++ whetstone/whetstone_suite_test.go
@@ -1,13 +1,31 @@
 package whetstone_test
 
 import (
+	"testing"
+
+	Bbs "github.com/cloudfoundry-incubator/runtime-schema/bbs"
+	"github.com/cloudfoundry/gunk/timeprovider"
+	"github.com/cloudfoundry/storeadapter/etcdstoreadapter"
+	"github.com/cloudfoundry/storeadapter/workerpool"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"testing"
+	"github.com/pivotal-golang/lager/lagertest"
 )
+
+var bbs *Bbs.BBS
+
+const StackName = "lucid64"
 
 func TestWhetstone(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Whetstone Suite")
 }
+
+var _ = BeforeEach(func() {
+	adapter := etcdstoreadapter.NewETCDStoreAdapter([]string{"http://10.244.16.2:4001"}, workerpool.NewWorkerPool(20))
+
+	err := adapter.Connect()
+	Expect(err).ToNot(HaveOccurred())
+
+	bbs = Bbs.NewBBS(adapter, timeprovider.NewTimeProvider(), lagertest.NewTestLogger("test"))
+})
--- whetstone/whetstone_test.go
+++ whetstone/whetstone_test.go
@@ -1,9 +1,88 @@
 package whetstone_test
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/cloudfoundry-incubator/runtime-schema/models"
+	"github.com/cloudfoundry-incubator/runtime-schema/models/factories"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Diego Edge", func() {
+	Context("when desiring a docker-based LRP", func() {
+		It("eventually runs on an executor", func() {
+			processGuid := factories.GenerateGuid()
+
+			repUrl := "http://127.0.0.1:20515"
+
+			err := bbs.DesireLRP(models.DesiredLRP{
+				Domain:      "whettest-stone-ever",
+				ProcessGuid: processGuid,
+				Instances:   1,
+				Stack:       "lucid64",
+				RootFSPath:  "docker:///onsi/grace-busybox",
+				Routes:      []string{"grace.10.244.0.34.xip.io"},
+				MemoryMB:    128,
+				DiskMB:      1024,
+				Ports: []models.PortMapping{
+					{ContainerPort: 8080},
+				},
+				Actions: []models.ExecutorAction{
+					models.Parallel(
+						models.ExecutorAction{
+							models.RunAction{
+								Path: "/grace",
+								Env: []models.EnvironmentVariable{
+									{Name: "VCAP_APPLICATION", Value: `{"instance_index":0}`},
+									{Name: "PORT", Value: "8080"},
+								},
+							},
+						},
+						models.ExecutorAction{
+							models.MonitorAction{
+								Action: models.ExecutorAction{
+									models.RunAction{ //The spy. Is this container healthy? running on 8080?
+										Path: "echo",
+										Args: []string{"all good"},
+									},
+								},
+								HealthyThreshold:   1,
+								UnhealthyThreshold: 1,
+								HealthyHook: models.HealthRequest{ //Teel the rep where to call back to on exit 0 of spy
+									Method: "PUT",
+									URL: fmt.Sprintf(
+										"%s/lrp_running/%s/PLACEHOLDER_INSTANCE_INDEX/PLACEHOLDER_INSTANCE_GUID",
+										repUrl,
+										processGuid,
+									),
+								},
+							},
+						},
+					),
+				},
+			})
+
+			Expect(err).To(BeNil())
+			Eventually(AppCheckerError("http://grace.10.244.0.34.xip.io")).ShouldNot(HaveOccurred())
+
+		})
+	})
 
 })
+
+func AppCheckerError(route string) func() error {
+	return func() error {
+		resp, err := http.Get(route)
+		if err != nil {
+			return err
+		}
+
+		if resp.StatusCode != 200 {
+			return fmt.Errorf("Status code %d should be 200", resp.StatusCode)
+		}
+
+		return nil
+	}
+}
