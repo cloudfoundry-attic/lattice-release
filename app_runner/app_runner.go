a0a6fe57f4d359af381f362a9a85221a090a24d2
David Julia <djulia@pivotal.io>
2014-12-16 17:40:34 -0600

    Allow scaling an app back up after stopping it
    Signed-off-by: Chelsea Troy <ctroy@pivotal.io>

--- app_runner/app_runner.go
+++ app_runner/app_runner.go
@@ -38,10 +38,8 @@ func (appRunner *appRunner) StartDockerApp(name, dockerImagePath, startCommand s
 }
 
 func (appRunner *appRunner) ScaleApp(name string, instances int) error {
-	if _, desiredLRPsCount, err := appRunner.desiredLRPInfo(name); err != nil {
+	if _, _, err := appRunner.desiredLRPInfo(name); err != nil {
 		return err
-	} else if desiredLRPsCount == 0 {
-		return newAppNotStartedError(name)
 	}
 
 	return appRunner.updateLrp(name, instances)
