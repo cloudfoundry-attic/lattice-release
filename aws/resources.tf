2019d500f570739bdb1b7613d34a161d0142b4e0
Ferran Rodenas <frodenas@gmail.com>
2015-01-04 18:56:39 -0800

    Enable DNS hostnames for AWS templates

--- aws/resources.tf
+++ aws/resources.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "lattice" {
     cidr_block = "${var.aws_vpc_cidr_block}"
     enable_dns_support = true
+    enable_dns_hostnames = true
     tags {
         Name = "lattice"
     }
