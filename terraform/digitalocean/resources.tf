26e8c0acb8879e23451aa191c99475c1c494bad4
David Wadden <dwadden@pivotal.io>
2015-02-02 15:16:53 -0600

    Set the -externalIP flag on garden-linux
      + This allows connecting to containers from outside of the cell.
    
    [Finishes #86461402]
    
    Signed-off-by: David Julia <djulia@pivotal.io>

--- terraform/digitalocean/resources.tf
+++ terraform/digitalocean/resources.tf
@@ -42,6 +42,7 @@ resource "digitalocean_droplet" "lattice-coordinator" {
             "sudo sh -c 'echo \"LATTICE_PASSWORD=${var.lattice_password}\" >> /var/lattice/setup/lattice-environment'",
             "sudo sh -c 'echo \"CONSUL_SERVER_IP=${digitalocean_droplet.lattice-coordinator.ipv4_address}\" >> /var/lattice/setup/lattice-environment'",
             "sudo sh -c 'echo \"SYSTEM_DOMAIN=${digitalocean_droplet.lattice-coordinator.ipv4_address}.xip.io\" >> /var/lattice/setup/lattice-environment'",
+            "sudo sh -c 'echo \"GARDEN_EXTERNAL_IP=$(hostname -i)\" >> /var/lattice/setup/lattice-environment'",
         ]
     }
 
