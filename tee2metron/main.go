4e994f09f1077ff5054acf8890b914b79ba920ee
David Julia <djulia@pivotal.io>
2015-03-05 09:24:49 -0600

    Display useful errors when bad flags or bad commands are passed into tee2metron
    [Finishes #83694856]

--- tee2metron/main.go
+++ tee2metron/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+    "fmt"
 )
 
 var dropsondeDestination, sourceInstance string
@@ -20,19 +21,34 @@ func init() {
 		"",
 		`where to stream logs to in the form of hostname:port
     eg. -dropsondeDestination=127.0.0.1:3457
-    `) //TODO: VALIDATE FLAG PASSED IN!!
+    `)
 
 	flag.StringVar(
 		&sourceInstance,
 		"sourceInstance",
 		"",
 		"The label for the log source instance that shows up when consuming the stream",
-	) //TODO: VALIDATE FLAG PASSED IN!!
+	)
 	flag.Parse()
 }
 
 func main() {
+    if dropsondeDestination == ""{
+        fmt.Println("dropsondeDestination flag is required")
+        os.Exit(1)
+    }
+    if sourceInstance == ""{
+        fmt.Println("sourceInstance flag is required")
+        os.Exit(1)
+    }
+
 	args := flag.Args()
+
+    if len(args) == 0 {
+        fmt.Println("Command not specified!")
+        fmt.Println("Usage: tee2metron -dropsondeDestionation=127.0.0.1:3457 -sourceInstance=lattice-cell-21 COMMAND")
+        os.Exit(3)
+    }
 	err := dropsonde.Initialize(dropsondeDestination, sourceInstance, args[0])
 
 	if err != nil {
@@ -52,13 +68,14 @@ func main() {
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdout = stdoutTeeWriter
 	cmd.Stderr = stderrTeeWriter
-	go logs.ScanLogStream(latticeDebugStreamId, args[0], sourceInstance, dropsondeStdoutReader) //TODO: UNIT TEST THIS, integration test won't catch appId
+	go logs.ScanLogStream(latticeDebugStreamId, args[0], sourceInstance, dropsondeStdoutReader)
 	go logs.ScanErrorLogStream(latticeDebugStreamId, args[0], sourceInstance, dropsondeStderrReader)
 
 	err = cmd.Start()
 	if err != nil {
-		panic(err) //TODO: COULD USE A UNIT TEST!
-	}
+        fmt.Println(err)
+        os.Exit(3)
+    }
 
 	cmd.Wait()
 }
--- tee2metron/main_test.go
+++ tee2metron/main_test.go
@@ -28,7 +28,7 @@ var _ = Describe("tee2metron", func() {
 		gexec.CleanupBuildArtifacts()
 	})
 
-	It("prints stdout to stdout and stderr to stderr, while logging a stdout log message to ", func() {
+	It("prints stdout and stderr and streams them to metron", func() {
 		metronReceivedBuffer, port := startFakeMetron()
 		dropsondeDestinationFlag := "-dropsondeDestination=127.0.0.1:" + port
 		command := exec.Command(tee2MetronPath, dropsondeDestinationFlag, "-sourceInstance=lattice-cell-123", chattyProcessPath, "chattyArg1", "chattyArg2", "-chattyFlag")
@@ -42,10 +42,59 @@ var _ = Describe("tee2metron", func() {
 		Eventually(gbytes.BufferWithBytes(*metronReceivedBuffer)).Should(gbytes.Say(chattyProcessPath))
 		Eventually(gbytes.BufferWithBytes(*metronReceivedBuffer)).Should(gbytes.Say("lattice-debug"))
 		Eventually(gbytes.BufferWithBytes(*metronReceivedBuffer)).Should(gbytes.Say("lattice-cell-123"))
-		Eventually(gbytes.BufferWithBytes(*metronReceivedBuffer)).Should(gbytes.Say("Hi from stdout. My args are: [chattyArg1 chattyArg2 -chattyFlag]"))
-		Eventually(gbytes.BufferWithBytes(*metronReceivedBuffer)).Should(gbytes.Say("Oopsie from stderr"))
+		Eventually(gbytes.BufferWithBytes(*metronReceivedBuffer), 5).Should(gbytes.Say("Hi from stdout. My args are: [chattyArg1 chattyArg2 -chattyFlag]"))
+		Eventually(gbytes.BufferWithBytes(*metronReceivedBuffer), 5).Should(gbytes.Say("Oopsie from stderr"))
 	})
 
+    Context("With a bad command", func(){
+        Context("when the command is missing", func(){
+            It("prints and error message and exits", func(){
+                command := exec.Command(tee2MetronPath, "-dropsondeDestination=127.0.0.1:4000", "-sourceInstance=lattice-cell-123")
+                session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+                Expect(err).ToNot(HaveOccurred())
+                Eventually(session.Out).Should(gbytes.Say("Command not specified!"))
+                Eventually(session.Out).Should(gbytes.Say("Usage: tee2metron -dropsondeDestionation=127.0.0.1:3457 -sourceInstance=lattice-cell-21 COMMAND"))
+                Eventually(session.Exited).Should(BeClosed())
+                Expect(session.ExitCode()).To(Equal(3))
+            })
+        })
+        Context("when there is an error executing the command", func(){
+            It("prints and error message and exits", func(){
+                command := exec.Command(tee2MetronPath, "-dropsondeDestination=127.0.0.1:4000", "-sourceInstance=lattice-cell-123", "do-the-fandango-for-me")
+                session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+                Expect(err).ToNot(HaveOccurred())
+                Eventually(session.Out).Should(gbytes.Say(`exec: "do-the-fandango-for-me": executable file not found in \$PATH`))
+                Eventually(session.Exited).Should(BeClosed())
+                Expect(session.ExitCode()).To(Equal(3))
+            })
+        })
+    })
+
+    Describe("Flags", func(){
+        Describe("-dropsondeDestination",func(){
+            It("is required", func() {
+                command := exec.Command(tee2MetronPath, "-sourceInstance=lattice-cell-123", chattyProcessPath)
+                session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+
+                Eventually(session.Buffer()).Should(gbytes.Say("dropsondeDestination flag is required"))
+                Expect(err).ToNot(HaveOccurred())
+                Eventually(session.Exited).Should(BeClosed())
+                Expect(session.ExitCode()).To(Equal(1))
+            })
+        })
+
+        Describe("-sourceInstance",func(){
+            It("is required", func() {
+                command := exec.Command(tee2MetronPath, "-dropsondeDestination=127.0.0.1:4000", chattyProcessPath)
+                session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+
+                Eventually(session.Buffer()).Should(gbytes.Say("sourceInstance flag is required"))
+                Expect(err).ToNot(HaveOccurred())
+                Eventually(session.Exited).Should(BeClosed())
+                Expect(session.ExitCode()).To(Equal(1))
+            })
+        })
+    })
 })
 
 func startFakeMetron() (metronReceivedBufferPtr *[]byte, port string) {
