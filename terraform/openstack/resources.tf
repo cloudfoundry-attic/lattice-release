0b7b2c5b69abbdd3b67b46b5393c3e239c53cfa4
Matt Johnson <matt@dhcp-10-61-98-219.cisco.com>
2015-04-24 11:15:41 +0100

    SG needs to be more open

--- terraform/openstack/resources.tf
+++ terraform/openstack/resources.tf
@@ -15,8 +15,8 @@ resource "openstack_compute_secgroup_v2" "lattice-sg" {
         cidr = "0.0.0.0/0"
     }
     rule {
-        from_port = 80
-        to_port = 80
+        from_port = 1
+        to_port = 65535
         ip_protocol = "tcp"
         cidr = "0.0.0.0/0"
     }
