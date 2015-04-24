cdd3328bc5ea482a4fcdaff3269511108359eee7
Matt Johnson <matt@dhcp-10-61-98-219.cisco.com>
2015-04-24 12:37:41 +0100

    UDP for Security Groups

--- terraform/openstack/resources.tf
+++ terraform/openstack/resources.tf
@@ -23,7 +23,25 @@ resource "openstack_compute_secgroup_v2" "lattice-sg" {
     rule {
         from_port = 1
         to_port = 65535
+        ip_protocol = "udp"
+        cidr = "0.0.0.0/0"
+    }
+    rule {
+        from_port = 1
+        to_port = 65535
         ip_protocol = "tcp"
+        self = true
+    }
+    rule {
+        from_port = 1
+        to_port = 65535
+        ip_protocol = "udp"
+        self = true
+    }
+    rule {
+        from_port = 1
+        to_port = 1
+        ip_protocol = "icmp"
         self = true
     }
 }
