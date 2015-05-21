6403ada4ef31c315bd56ddaf85082bd3a44ef035
Lattice Continuous Deployment <cf-diego-edge-eng+gocd@pivotal.io>
2015-05-21 13:22:37 +0000

    Bump Terraform Examples
            + Update terraform example files to point to source v0.2.4-141-gce4e677
            + Terraform now downloads Lattice v0.2.4-140-g179a390
    
            +Committed by GOCD- Run #837 of Pipeline: diego-edge, Stage: promote_and_publish, JOB: promote_and_publish

--- terraform/digitalocean/example/lattice.digitalocean.tf
+++ terraform/digitalocean/example/lattice.digitalocean.tf
@@ -1,5 +1,5 @@
 module "lattice-digitalocean" {
-    source = "github.com/cloudfoundry-incubator/lattice//terraform//digitalocean?ref=v0.2.4-137-g7277d54"
+    source = "github.com/cloudfoundry-incubator/lattice//terraform//digitalocean?ref=v0.2.4-141-gce4e677"
 
     # Digital Ocean API token
     do_token = "<CHANGE-ME>"
