42182ae0940da1f0f4fc48afd4c05653f9a2c519
Lattice Continuous Deployment <cf-diego-edge-eng+gocd@pivotal.io>
2015-07-06 21:50:14 +0000

    Bump Terraform Examples
            + Update terraform example files to point to source v0.2.6-6-gb3dd5a7
            + Terraform now downloads Lattice v0.2.6-5-gc5f6c98
    
            +Committed by GOCD- Run #902 of Pipeline: diego-edge, Stage: promote_and_publish, JOB: promote_and_publish

--- terraform/openstack/example/lattice.openstack.tf
+++ terraform/openstack/example/lattice.openstack.tf
@@ -1,5 +1,5 @@
 module "lattice-openstack" {
-    source = "github.com/cloudfoundry-incubator/lattice//terraform//openstack?ref=v0.2.6"
+    source = "github.com/cloudfoundry-incubator/lattice//terraform//openstack?ref=v0.2.6-6-gb3dd5a7"
 
     # OpenStack User Account
     openstack_access_key = "<CHANGE-ME>"
