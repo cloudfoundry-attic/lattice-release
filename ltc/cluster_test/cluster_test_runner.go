141f2af46c1567810da7acde39f183980579257a
Atul Kshirsagar <atul.kshirsagar@gmail.com>
2015-08-04 17:31:47 -0700

    Fix the compilation issues in cluster_test_runner
    Signed-off-by: Fermin Ordaz <fermin.ordaz@gmail.com>

--- ltc/cluster_test/cluster_test_runner.go
+++ ltc/cluster_test/cluster_test_runner.go
@@ -1,5 +1,6 @@
 package cluster_test
 
+import (
 	"bufio"
 	"encoding/json"
 	"errors"
@@ -475,7 +476,6 @@ func errorCheckForConnection(ip string, port uint16) func() error {
 		return nil
 	}
 }
-
 
 func errorCheckForRoute(route string) func() error {
 	fmt.Fprintln(getStyledWriter("test"), "Polling for the route", route)
