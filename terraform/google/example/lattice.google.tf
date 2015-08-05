35f4b4b89982d617763312ed782489a8cf74af6f
Lattice Continuous Deployment <cf-diego-edge-eng+gocd@pivotal.io>
2015-08-05 21:50:49 +0000

    Bump Terraform Examples
            + Update terraform example files to point to source v0.2.7-119-g3c5a491
            + Terraform now downloads Lattice v0.2.7-118-gd29858f
    
            +Committed by GOCD- Run #57 of Pipeline: lattice, Stage: promote_and_publish, JOB: promote_and_publish

--- terraform/google/example/lattice.google.tf
+++ terraform/google/example/lattice.google.tf
@@ -1,5 +1,5 @@
 module "lattice-google" {
-    source = "github.com/cloudfoundry-incubator/lattice//terraform//google?ref=v0.2.7-111-g9143fbe"
+    source = "github.com/cloudfoundry-incubator/lattice//terraform//google?ref=v0.2.7-119-g3c5a491"
 
     # Path to the JSON file used to describe your account credentials, downloaded from Google Cloud Console
     gce_account_file = "<CHANGE-ME>"
