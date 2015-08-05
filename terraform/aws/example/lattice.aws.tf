0bdb30805e45e2d96fa0c09f965da41771195ff1
Lattice Continuous Deployment <cf-diego-edge-eng+gocd@pivotal.io>
2015-08-05 12:08:57 +0000

    Bump Terraform Examples
            + Update terraform example files to point to source v0.2.7-104-g365a683
            + Terraform now downloads Lattice v0.2.7-103-gab5d376
    
            +Committed by GOCD- Run #48 of Pipeline: lattice, Stage: promote_and_publish, JOB: promote_and_publish

--- terraform/aws/example/lattice.aws.tf
+++ terraform/aws/example/lattice.aws.tf
@@ -1,5 +1,5 @@
 module "lattice-aws" {
-    source = "github.com/cloudfoundry-incubator/lattice//terraform//aws?ref=v0.2.7-97-ga8987de"
+    source = "github.com/cloudfoundry-incubator/lattice//terraform//aws?ref=v0.2.7-104-g365a683"
 
     # AWS access key
     aws_access_key = "<CHANGE-ME>"
