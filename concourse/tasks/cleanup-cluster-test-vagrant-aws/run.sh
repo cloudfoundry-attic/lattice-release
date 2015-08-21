d123380a42f96d019ce77fa934229e87db5438a7
David Wadden <dwadden@pivotal.io>
2015-08-21 10:18:39 -0500

    Add Vagrant box to cleanup-cluster-test-vagrant-aws task.
    [#101246342]
    
    Signed-off-by: Stephen Levine <slevine@pivotal.io>

--- concourse/tasks/cleanup-cluster-test-vagrant-aws/run.sh
+++ concourse/tasks/cleanup-cluster-test-vagrant-aws/run.sh
@@ -9,5 +9,6 @@ cat <<< "$AWS_SSH_PRIVATE_KEY" > "$AWS_SSH_PRIVATE_KEY_PATH"
 curl -LO https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb
 dpkg -i vagrant_1.7.4_x86_64.deb
 vagrant plugin install vagrant-aws
+vagrant box add lattice/ubuntu-trusty-64 --provider=aws
 
 ( cd $VAGRANT_TMP_DIR && vagrant destroy -f )
