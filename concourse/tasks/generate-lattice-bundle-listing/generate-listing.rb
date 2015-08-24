95c777f71d8e171182e1186d9deaf46063604a23
Damon Anderson <danderson@pivotal.io>
2015-08-24 15:24:42 -0500

    Generates listing for nightly bundles.
      + Attempts to publish listing to /nightly/index.html
      + Runs following bundle generation in publish-nightly job
    
    [#101834600]
    
    Signed-off-by: David Wadden <dwadden@pivotal.io>

---
+++ concourse/tasks/generate-lattice-bundle-listing/generate-listing.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 's3'
+require 'erb'
+require 'ostruct'
+
+service = S3::Service.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+						  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
+
+S3_BUCKET = 'lattice'
+
+objs_by_day = {}
+service.buckets.find(S3_BUCKET).objects.each do |obj|
+	next if !obj.key.start_with?("nightly/lattice-bundle") 
+	next if obj.key.start_with?('nightly/lattice-bundle-latest-')
+
+	version = /lattice-bundle-(.+?)-[^-]+\.zip$/.match(obj.key)[1]
+	arch = /-([^-]+)\.zip$/.match(obj.key)[1]
+
+	objs_by_day[obj.last_modified.to_date] ||= {}
+	objs_by_day[obj.last_modified.to_date][version] ||= []
+	objs_by_day[obj.last_modified.to_date][version] << {:arch => arch, :path => obj.key}
+end
+
+def binding_from_hash(hash)
+	OpenStruct.new(hash).instance_eval { binding }
+end
+
+erb = ERB.new(File.read('listing.html.erb'), 0, '>')
+print erb.result(binding_from_hash({:days => objs_by_day, :s3bucket => S3_BUCKET}))
---
+++ concourse/tasks/generate-lattice-bundle-listing/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x -e
+
+export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+
+gem install bundler
+
+OUTPUT=$PWD
+
+cd lattice/concourse/tasks/generate-lattice-bundle-listing
+
+bundler
+bundle exec ./generate-listing.rb > $OUTPUT/index.html
