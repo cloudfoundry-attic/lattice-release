aef3cea93506e8a1f3ad384bbec9df98ccb6e4f2
Stephen Levine <slevine@pivotal.io>
2015-09-24 21:45:54 -0400

    Bump diego to 0.1424.1 and refactor compile script
    - Also introduces garden-linux-release as dependency
    
    [#103211906]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- concourse/tasks/compile-lattice-tar/run.sh
+++ concourse/tasks/compile-lattice-tar/run.sh
@@ -2,26 +2,27 @@
 
 set -x -e
 
-export LATTICE_DIR=$PWD/lattice
-export DIEGO_RELEASE_DIR=$PWD/lattice/build/diego-release
-export GOPATH=$DIEGO_RELEASE_DIR
-export PATH=$GOPATH/bin:$PATH
+LATTICE_DIR=$PWD/lattice
 
-
-export LATTICE_VERSION=$(git -C $LATTICE_DIR describe --tags --always)
-export DIEGO_VERSION=$(cat $LATTICE_DIR/DIEGO_VERSION)
-export CF_VERSION=$(cat $LATTICE_DIR/CF_VERSION)
-export ROUTING_VERSION=$(cat $LATTICE_DIR/ROUTING_VERSION)
-export STACK_VERSION=$(cat $LATTICE_DIR/STACK_VERSION)
+DIEGO_VERSION=$(cat $LATTICE_DIR/DIEGO_VERSION)
+GARDEN_LINUX_VERSION=$(cat $LATTICE_DIR/GARDEN_LINUX_VERSION)
+CF_VERSION=$(cat $LATTICE_DIR/CF_VERSION)
+ROUTING_VERSION=$(cat $LATTICE_DIR/ROUTING_VERSION)
 
 if [ "$RELEASE" = true ]; then
-  export LATTICE_VERSION=$(cat $LATTICE_DIR/Version)
+  LATTICE_VERSION=$(cat $LATTICE_DIR/Version)
 fi
 
-pushd $DIEGO_RELEASE_DIR
+pushd $LATTICE_DIR/build/diego-release 
 	git checkout $DIEGO_VERSION
 	git clean -xffd
 	./scripts/update
+popd
+
+pushd $LATTICE_DIR/build/garden-linux-release 
+	git checkout $GARDEN_LINUX_VERSION
+	git clean -xffd
+  git submodule update --init --recursive
 popd
 
 pushd $LATTICE_DIR/build/cf-release
@@ -36,20 +37,5 @@ pushd $LATTICE_DIR/build/cf-routing-release
   ./scripts/update
 popd
 
-$LATTICE_DIR/cluster/scripts/compile \
-    $LATTICE_DIR/build/lattice-build \
-    $LATTICE_DIR/build/diego-release \
-    $LATTICE_DIR/build/cf-release \
-    $LATTICE_DIR/build/cf-routing-release \
-    $LATTICE_DIR
-
-echo $LATTICE_VERSION > $LATTICE_DIR/build/lattice-build/common/LATTICE_VERSION
-echo $DIEGO_VERSION > $LATTICE_DIR/build/lattice-build/common/DIEGO_VERSION
-echo $CF_VERSION > $LATTICE_DIR/build/lattice-build/common/CF_VERSION
-echo $ROUTING_VERSION > $LATTICE_DIR/build/lattice-build/common/ROUTING_VERSION
-echo $STACK_VERSION > $LATTICE_DIR/build/lattice-build/common/STACK_VERSION
-
-tar czf $LATTICE_DIR/build/lattice.tgz -C $LATTICE_DIR/build lattice-build
-
-mv $LATTICE_DIR/build/lattice.tgz $LATTICE_DIR/build/lattice-${LATTICE_VERSION}.tgz
+$LATTICE_DIR/cluster/scripts/compile $LATTICE_DIR/build/lattice-${LATTICE_VERSION}.tgz $LATTICE_VERSION
 
